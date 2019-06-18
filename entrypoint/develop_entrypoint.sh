7d5fffee4a35cd3882eca6b5a62a64a2f59b501f
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2019-06-18 12:22:09 +0300

    First COmmit

---
+++ entrypoint/develop_entrypoint.sh
@@ -0,0 +1,38 @@
+ #!/bin/sh
+
+USER_ID=$(id -u developer)
+GROUP_ID=$(id -g developer)
+
+echo "Setting the correct user and group id for shell use"
+if [ ${DOCKER_UID} != ${USER_ID} ]; then
+  usermod -u ${DOCKER_UID} developer
+fi
+
+if [ ${DOCKER_GID} != ${GROUP_ID} ]; then
+  groupmod -g ${DOCKER_GID} developer
+fi
+
+echo "Setup xdebug"
+cp ${XDEBUG_CONF_FILE}.orig ${XDEBUG_CONF_FILE}
+
+if [ -z ${XDEBUG_HOST} ]; then
+    ip=$(netstat -rn | grep "^0.0.0.0 " | cut -d " " -f10)
+    XDEBUG_HOST=${ip}
+fi
+
+echo "xdebug.remote_host=${XDEBUG_HOST}" >> ${XDEBUG_CONF_FILE}
+echo "xdebug.repomote_port=${XDEBUG_PORT}" >> ${XDEBUG_CONF_FILE}
+
+if [ XDEBUG_DBGP = TRUE ]; then
+    echo "xdebug.remote.handler=dbgp" >> ${XDEBUG_CONF_FILE}
+fi
+
+if [ ! -z "${XDEBUG_IDE_KEY}" ]; then
+    echo "xdebug.idekey=\"${XDEBUG_IDE_KEY}\"" >> ${XDEBUG_CONF_FILE}
+fi
+
+echo "Fixing execution permissions"
+find /home/developer/code -iname "*.php" | xargs chmod 777
+
+echo "Launch application"
+exec "$@"
