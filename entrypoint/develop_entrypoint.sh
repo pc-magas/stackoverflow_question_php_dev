a4b967dc30de1719bdcd80bcf1b659729d43bb32
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2019-06-18 12:57:39 +0300

    Uncommited COmmits

--- entrypoint/develop_entrypoint.sh
+++ entrypoint/develop_entrypoint.sh
@@ -12,25 +12,6 @@ if [ ${DOCKER_GID} != ${GROUP_ID} ]; then
   groupmod -g ${DOCKER_GID} developer
 fi
 
-echo "Setup xdebug"
-cp ${XDEBUG_CONF_FILE}.orig ${XDEBUG_CONF_FILE}
-
-if [ -z ${XDEBUG_HOST} ]; then
-    ip=$(netstat -rn | grep "^0.0.0.0 " | cut -d " " -f10)
-    XDEBUG_HOST=${ip}
-fi
-
-echo "xdebug.remote_host=${XDEBUG_HOST}" >> ${XDEBUG_CONF_FILE}
-echo "xdebug.repomote_port=${XDEBUG_PORT}" >> ${XDEBUG_CONF_FILE}
-
-if [ XDEBUG_DBGP = TRUE ]; then
-    echo "xdebug.remote.handler=dbgp" >> ${XDEBUG_CONF_FILE}
-fi
-
-if [ ! -z "${XDEBUG_IDE_KEY}" ]; then
-    echo "xdebug.idekey=\"${XDEBUG_IDE_KEY}\"" >> ${XDEBUG_CONF_FILE}
-fi
-
 echo "Fixing execution permissions"
 find /home/developer/code -iname "*.php" | xargs chmod 777
 
